6da8353e65e6d3e41c8ea8dca28b8c5e0e92049a
amr <amrmuhamed9@gmail.com>
2020-11-13 23:21:39 +0200

    Application Run

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,1 +1,21 @@
 <?php
+
+
+class Application{
+    /**
+     * Application Construct
+     **/
+    private function __construct()
+    {
+
+    }
+
+    /**
+    * Application Run
+     * @return void
+    **/
+    public function run(){
+        echo  "Say Hi";
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -1,10 +1,36 @@
 <?php
 
-if (!isset($_SERVER['PATH_INFO']))
-{
-    echo "Home page";
-    exit();
-}
+    /**
+         *  Luxury-php web Framework v.1.0.0
+         *  @author : Amr Muhamed <amrmuhamed9@gmail.com>
+    **/
 
-print "The request path is : ".$_SERVER['PATH_INFO'];
-?>
+    /**
+    *---------------------------------------------------------------------
+    *   Register Autoloader
+    *---------------------------------------------------------------------
+    *   load the autoloader that will generated class that will be used
+    *
+    **/
+
+require __DIR__. "/../vendor/autoload.php";
+
+    /**
+    *---------------------------------------------------------------------
+    *    Bootstrap Application
+    *---------------------------------------------------------------------
+    *    Bootstrap the application and  do action from framework*
+    *
+    **/
+
+require __DIR__. "/../bootstrap/app.php";
+
+    /**
+     * ------------------------------------------------
+     *      Run The Application
+     * ------------------------------------------------
+     *  Handel the request and send the responce
+     *
+     **/
+
+$app = Application::run();
